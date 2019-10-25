8c844401db19c77c3bd89c823ea0c28e9776a308
Raj Krishna Srivastava <raj@Rajs-MacBook-Air.local>
2019-10-25 17:01:08 +0530

    added modules to be implemented

---
+++ retrieve_email.py
@@ -0,0 +1,4 @@
+def get_emails(userid,password):
+    #the function should take take user name and password and return the contents of inbox
+    #in a format that we can extract mails from it
+    raise NotImplemented
---
+++ text_to_speech.py
@@ -0,0 +1,3 @@
+def t2s(text):
+    #the function should take a string in text and we should get an output through the speakers
+    raise NotImplemented
