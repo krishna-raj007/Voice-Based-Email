baa4ae25a2479c2e8fd06dd622b1436314b137e9
Raj Krishna Srivastava <raj@Rajs-MacBook-Air.local>
2019-10-28 14:48:43 +0530

    added text_to_speech

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,3 +1,12 @@
+from gtts import gTTS 
+import os 
 def t2s(text):
     #the function should take a string in text and we should get an output through the speakers
-    raise NotImplemented
+    language = 'en'
+    myobj = gTTS(text=text, lang=language, slow=False)
+    myobj.save("VoiceFiles/audio.mp3")
+    os.system("mpg321 VoiceFiles/audio.mp3")
+    os.remove("VoiceFiles/audio.mp3")
+    #raise NotImplemented
+
+
--- voiceBasedEmail.py
+++ voiceBasedEmail.py
@@ -1,26 +1,40 @@
 import smtplib
 import speech_recognition as sr
-
+import retrieve_email
+import text_to_speech as ts
+email_id = "lionsofmirzapur699@gmail.com"
+password = "LionsOfMirzapur"
 
 srObj = sr.Recognizer()
-print("Welcome to your email account. Please enter your choice:")
-print("1: Compose Mail")
-print("2: Read from inbox")
+ts.t2s("Welcome to your email account. Please enter your choice:")
+ts.t2s("1: Compose Mail")
+ts.t2s("2: Read from inbox")
 with sr.Microphone() as source:
     choice_audio=srObj.listen(source)
 
 choice=srObj.recognize_google(choice_audio)
 #put a check if only one of the two choices are present or not
-
+print ("You said:", choice)
+ts.t2s("You said:"+choice)
 if choice == '1':
-    print ("Your Choice is 1. Speak your message")
+    ts.t2s(" Speak your message")
     with sr.Microphone() as source:
         choice_audio=srObj.listen(source)
     msg=srObj.recognize_google(choice_audio)
-    print ("Your message is:",msg)
-    # Say confirm or go back
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login("rajsrivastava871@gmail.com", "9721377988Krishna@7")
-    server.sendmail("rajsrivastava871@gmail.com", "krishna_raj007@yahoo.in", msg)
-    print ("your message has been sent")
+    ts.t2s("Your message is:"+msg)
+    ts.t2s("If corect say yes else say no")
+    with sr.Microphone() as source:
+        choice_audio=srObj.listen(source)
+    choice=srObj.recognize_google(choice_audio)
+    if choice == "yes":
+        ts.t2s("You said Yes")
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(email_id, password)
+        server.sendmail(email_id, "gaurav.singh.atoria@gmail.com", msg)
+        ts.t2s("your message has been sent")
+
+
+##############################################
+def mouse_click():
+    raise NotImplemented
