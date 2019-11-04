61200863f268b2e5ab7c95815f1d147af76c7a04
krishna-raj007 <krishna_raj007@yahoo.in>
2019-11-05 01:43:29 +0530

    foward and reply

--- send_mail.py
+++ send_mail.py
@@ -18,11 +18,11 @@ class Gmail_smtp:
         # ts.t2s("If corect say yes else say no")
         print("Your message is:" + msg)
         print("If corect say yes else say no")
-        ts.t2s("Your message has been recorded. Say 1 to send mail and 2 to save")
-        choice=ts.get_command()
-        if choice == "1":
+        ts.t2s("Your message has been recorded. Say send to send this mail and save to save it to drafts")
+        choice = ts.get_command()
+        if choice == "send":
             self.send_mail(msg)
-        elif choice == "2":
+        elif choice == "save":
             self.save_mail(msg)
         return
 
@@ -35,7 +35,7 @@ class Gmail_smtp:
         ts.t2s(" Speak recepient's email id")
         print(" Speak recepient's email id")
 
-        receiever=ts.get_command()
+        receiever=ts.get_email()
         print(receiever)
         reciever = "gaurav.singh.atoria@gmail.com"
         ts.t2s("Your message reciever is:" + reciever)
@@ -48,7 +48,43 @@ class Gmail_smtp:
         ts.t2s("your message has been sent")
         return
 
+    def forward_mail(self, msg):
+        ts.t2s(" Speak recepient's email id")
+        print(" Speak recepient's email id")
 
+        receiever = ts.get_email()
+        print(receiever)
+        reciever = "gaurav.singh.atoria@gmail.com"
+        ts.t2s("Your message reciever is:" + reciever)
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        print("Connected to gmail server")
+        server.starttls()
+        server.login(self.userid, self.password)
+        print("logged in")
+        server.sendmail(self.userid, "gaurav.singh.atoria@gmail.com", msg)
+        ts.t2s("your message has been sent")
+        return
 
+    def reply_mail(self,recepient_id):
+        ts.t2s("Please provide your reply")
+        print(" Speak your reply")
+        reply = ts.get_command()
+        # ts.t2s("Your message is:"+msg)
+        # ts.t2s("If corect say yes else say no")
+        #print("Your message is:" + reply)
+        print("If corect say yes else say no")
+        ts.t2s("Your reply has been recorded. Say send to send the reply")
+        choice = ts.get_command()
+        if choice == "send":
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            print("Connected to gmail server")
+            server.starttls()
+            server.login(self.userid, self.password)
+            print("logged in")
+            server.sendmail(self.userid, recepient_id, reply)
+            ts.t2s("your reply has been sent to " + recepient_id)
+            return
+        else:
+            ts.t2s("Reply not sent")
     # def save_mail(self, msg):
     #     pass
--- text_to_speech.py
+++ text_to_speech.py
@@ -15,18 +15,61 @@ def t2s(text):
 
 
 def get_command():
-
-    while (1):
-        t2s("speak now")
+    while(1):
+        while (1):
+            t2s("Speak now")
+            try:
+                with sr.Microphone() as source:
+                    choice_audio = srObj.listen(source)
+                voice_inp = srObj.recognize_google(choice_audio)
+                break
+            except:
+                t2s("I dint get you . Could you please repeat")
+                print("I dint get you . Could you please repeat")
+            # put a check if only one of the two choices are present or not
+        print("You said:", voice_inp)
+        t2s("You said:" + voice_inp + ". Say yes to confirm and no to try again ")
         try:
             with sr.Microphone() as source:
                 choice_audio = srObj.listen(source)
-            voice_inp = srObj.recognize_google(choice_audio)
-            break
+            confirmation = srObj.recognize_google(choice_audio)
+            if confirmation == "yes":
+                break
+            else:
+                t2s("You said no. Kindly repeat you message")
         except:
-            t2s("Pardon me. Could you please repeat")
-            print("Pardon me. Could you please repeat")
-        # put a check if only one of the two choices are present or not
-    print("You said:", voice_inp)
-    t2s("You said:" + voice_inp)
+            t2s("Some problem occured . Kindly repeat you message")
+    print ("returning message")
+    return voice_inp
+
+
+def get_email():
+    while(1):
+        while (1):
+            t2s("Speak now")
+            try:
+                with sr.Microphone() as source:
+                    choice_audio = srObj.listen(source)
+                voice_inp = srObj.recognize_google(choice_audio)
+                break
+            except:
+                t2s("I dint get you . Could you please repeat")
+                print("I dint get you . Could you please repeat")
+            # put a check if only one of the two choices are present or not
+        print("You said:", voice_inp)
+        voice_inp.replace("dot", ".")
+        voice_inp.replace("underscore", "_")
+        voice_inp.replace("at", "@")
+        voice_inp.replace(" ", "")
+        t2s("You said:" + voice_inp + ". Say yes to confirm and no to try again ")
+        try:
+            with sr.Microphone() as source:
+                choice_audio = srObj.listen(source)
+            confirmation = srObj.recognize_google(choice_audio)
+            if confirmation == "yes":
+                break
+            else:
+                t2s("You said no. Kindly repeat you message")
+        except:
+            t2s("You said no. Kindly repeat you message")
     return voice_inp
