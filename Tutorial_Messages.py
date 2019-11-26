a0fea7200edb9c2f4a2d718b0ff1b7e5d9a21fb4
vinod <vinod@cse.iitb.ac.in>
2019-11-26 21:42:33 +0530

    major change- full voice control

--- Tutorial_Messages.py
+++ Tutorial_Messages.py
@@ -1,13 +1,11 @@
 import text_to_speech as ts
 # class Tutorial_Messages:
 def dashboard_msg():
-    ts.t2s("Welcome to your email account. Please listen your choices , Single left click to compose,  Single right to inbox, Double right click to logout")
+    ts.t2s("Welcome to your email account.   Please listen your choices.   Compose to  compose.  Inbox to access your  inbox. Logout to  logout form this account ")
 
 
 def inbox_msg():
-    ts.t2s("Welcome to your inbox. Please listen your choice , Single left click to read all mails,  right click to go to dashboard")
+    ts.t2s("Welcome to your inbox. Please listen your choice.  Read all k to read all mails. Dashboard to go to your mail dashboard.")
 
 def readAll_msg():
-	ts.t2s("reading all mails please give command after each mail information and your commands are ")
-def readAll_cmds():
-    ts.t2s("read to read complete message, next to read next mail , forward to forward mail , Delete to delete mail,  reply to reply mail , stop to stop reading all mails and go to inbox, and help to listen commands")
+    ts.t2s("reading all mails give command after each mail information after you listen speak now, read to read complete message next to read next mail forward to forward mail Delete to delete mail reply to reply mail stop to stop reading all mails and go to inbox")
--- voiceBasedEmail.py
+++ voiceBasedEmail.py
@@ -1,130 +1,85 @@
-#importing libraries
+# importing libraries
 
 import speech_recognition as sr
-from tkinter import *
-from tkinter.ttk import *
-
-#importing other modules
-
 import retrieve_email as rm
 import text_to_speech as ts
 import send_mail as sm
 import Tutorial_Messages as tm
 
-login = 'vinodassignmentiitb@gmail.com'
-password = 'assignments@vinod'
+account, smtp =0,0
+def initialise():
+    file1 = open("userIDPassword.txt", "r")
+    login, password = file1.readline().split(",")
+    print(login, password)
+    file1.close()
+    global account
+    account = rm.Gmail_imap(login, password)
+    global smtp
+    smtp = sm.Gmail_smtp(login, password)
 
-account=rm.Gmail_imap(login,password)
-smtp=sm.Gmail_smtp(login,password)
-root = Tk() 
-window_height=root.winfo_screenheight()
-window_width=root.winfo_screenwidth()
 def get_credentials():
     ts.t2s("Speak your email id")
-    userid=ts.get_command()
+    userid = ts.get_command()
     ts.t2s("speak your password")
-    password=ts.get_command()
-    print("userid=",userid,"pass=",password)
+    password = ts.get_command()
+    print("userid=", userid, "pass=", password)
 
-def dashboard_gui():
+
+def readAll():
+    print("readAll")
+    tm.readAll_msg()
+    l = account.get_allmail()
+    for i in l:
+        mail = i.from_addr + " " + i.title
+        ts.t2s(mail)
+        cmd = ts.get_command()
+        if cmd == "read":
+            mail = i.body
+            ts.t2s(mail)
+        elif cmd == "next":
+            continue
+        elif cmd == "forward":
+            smtp.forward_mail(i.body)
+        elif cmd == "delete":
+            pass
+        elif cmd == "reply":
+            smtp.reply_mail(i.from_addr)
+        elif cmd == "stop":
+            break
+
+
+
+def inbox():
+    # creates tkinter window or root window
+    tm.inbox_msg()
+
+    cmd = ts.get_command()
+    if cmd == "readall":
+        readAll()
+    elif cmd == "dashboard":
+        dashboard()
+
+
+
+def dashboard():
     # creates tkinter window or root window 
     tm.dashboard_msg()
-    # function to be called when button-2 of mouse is pressed 
-    dashboard = Frame(root, height = window_height, width = window_width)
-    def left_compose(event): 
-        #print('Button-1 left pressed at x = % d, y = % d'%(event.x, event.y))
+    cmd = ts.get_command()
+    if cmd == "inbox":
+        inbox()
+    elif cmd == "compose":
         smtp.compose_mail()
-      
-    ## function to be called when button-1 is double clocked 
-    def right_inbox(event): 
-        #print('Double left clicked at x = % d, y = % d'%(event.x, event.y))
-        dashboard.destroy()
-        inbox_gui()
-        #rm.get_inbox() 
-    def dright_logout(event): 
-        print('Double right clicked at x = % d, y = % d'%(event.x, event.y))
-        #rm.logout()
-     
- 
-      
-    # these lines are binding mouse 
-    # buttons with the Frame widget 
-    dashboard.bind('<Button-1>', left_compose) 
-    #dashboard.bind('<Button-3>', right_send) 
-    dashboard.bind('<Button-3>', right_inbox)
-    dashboard.bind('<Double-Button-3>',dright_logout)
-      
-    dashboard.pack() 
-      
-    mainloop() 
-    #raise NotImplemented
-#####################################################################################
-def inbox_gui():
-    # creates tkinter window or root window 
-    tm.inbox_msg()
-    inbox_window = Frame(root, height = window_height, width = window_width)
-    # function to be called when button-2 of mouse is pressed 
-    # def readUnseen(event): 
-    #     print('Button-1 left pressed at x = % d, y = % d'%(event.x, event.y))
-    #     print("readUnseen")
-    #     l=account.get_unseenmail()
-    #     for i in l:
-    #     	mail=i.from_addr+" "+i.title+" "+i.body
-    #     	ts.t2s(mail)        	
-        	#readMail_brief(i)      
-    # function to be called when button-3 of mouse is pressed 
-    # def searchmail(event): 
-    #     print('Button-3 right pressed at x = % d, y = % d'%(event.x, event.y))
-    #     print("searchmail")
-      
-    ## function to be called when button-1 is double clocked 
-    def readAll(event): 
-    	cmds=["read","forward","reply","next","stop","delete","help"]
-    	#print('Double left clicked at x = % d, y = % d'%(event.x, event.y))
-    	print("readAll")
-    	tm.readAll_msg()
-    	tm.readAll_cmds()
-    	l=account.get_allmail()
-    	for i in l:
-        	mail=i.from_addr+" "+i.title
-        	ts.t2s(mail)
-        	cmd=ts.get_command(cmds)
-        	if cmd=="read":
-        		mail=i.body
-        		ts.t2s(mail)
-        	elif cmd=="next":
-        		continue
-        	elif cmd=="forward":
-        		smtp.forward_mail(i.body)
-        	elif cmd=="delete":
-        		pass
-        	elif cmd=="reply":
-        		smtp.reply_mail(i.from_addr)
-        	elif cmd=="stop":
-        		break
-        	elif cmd=="help":
-        		tm.readAll_cmds()
-        	else:
-        		ts.t2s("no command given  reading next mail")
-    def gotodashboard(event): 
-        #print('Double right clicked at x = % d, y = % d'%(event.x, event.y))
-        print("other")
-        inbox_window.destroy()
-        dashboard_gui()     
-      
-    # these lines are binding mouse 
-    # buttons with the Frame widget 
-    # inbox_window.bind('<Button-1>', readUnseen) 
-    # inbox_window.bind('<Button-3>', searchmail) 
-    inbox_window.bind('<Button-1>', readAll) 
-    inbox_window.bind('<Button-3>',gotodashboard)
-      
-    inbox_window.pack() 
-    mainloop() 
-    #raise NotImplemented
+    elif cmd == "logout":
+        rm.logout()
+  
+def get_user_response():
+    pass
+
+
 def main():
-    #initialise()
-    #get_credentials()
-    dashboard_gui()
-    
+    initialise()
+    # get_credentials()
+    dashboard()
+
+
 main()
